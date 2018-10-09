4c3793cef7c727a9a27eeb2488c140a5a29399f3
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-09 09:09:02 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
 
 
+class Book
+
+  attr_accessor :genre, :page_count, :author
+  attr_reader :title
+
+  def initialize(title)
+    @title = title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,10 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :condition, :size, :material, :color, :brand
+  def initialize(brand)
+    @brand = brand
+  end
+  def cobble
+    puts "Your shoe is as good as new!"
+    self.condition = 'new'
+  end
+end
