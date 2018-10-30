aed388df913568c64db788971a0d28d5fe3ffd9a
Sung Bae <singsang2@Sungs-iMac.local>
2018-10-30 13:14:56 -0400

    book and shoe completed

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,15 @@
 # Make your shoe class here!
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
+  def initalize(brand)
+    @brand = brand
+  end
+
+  def cobble
+    puts "the shoe has been repaired!"
+    self.condition = "new"
+  end
+
+end
