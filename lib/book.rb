6415e3aec70260c582d15a8df4f6bc5f4cf24a9e
karen ercoli <karenercoli@gmail.com>
2018-11-27 20:43:53 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,26 @@
+class Book
+  def initialize(title)
+    @title = title
+  end
+  def title
+    @title
+  end
+  def author=(author)
+    @author = author
+  end
 
-
+  def author
+    @author
+  end
+  def page_count=(page_count)
+    @page_count = page_count
+  end
+  def page_count
+    @page_count
+  end
+  attr_accessor :genre
+def turn_page
+  puts "Flipping the page...wow, you read fast!"
+end
+end
+ 
