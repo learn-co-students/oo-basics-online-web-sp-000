80cca2fe68cf95ba57898ae1e8f84c2597c1aeb8
Aaron Berns <anberns@gmail.com>
2019-01-10 13:58:09 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,21 @@
-
+class Book 
+  
+  #automatic get and set for attributes
+  attr_accessor :author, :page_count, :genre
+  
+  def initialize(title)
+    @title = title
+  end
+  
+  
+  # getter
+  def title
+    @title 
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+  
+end
 
