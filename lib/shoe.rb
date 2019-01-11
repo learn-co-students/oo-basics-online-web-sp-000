441830879f5912940b83b9a8ad4ba00e4ffa4532
Diana Badas <33879699+DianaBaRo@users.noreply.github.com>
2019-01-11 12:43:40 +0000

    Using accessor and reader

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,4 +1,19 @@
 class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+end
+
+////////////
+
+class Shoe
 
   def initialize(brand)
     @brand = brand
