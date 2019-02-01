81ff9581c2d28b5bb1b78b0f5e6fa6193faf1cc3
David Gilbert <dagilbert333@gmail.com>
2019-02-01 07:23:29 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,12 +1,9 @@
 class Book 
   attr_accessor :author, :page_count, :genre
+  attr_reader :title
   
   def initialize(title)
     @title = title  
-  end
-  
-  def title
-    @title
   end
   
   def turn_page
