f70d5005e7c8e4418951d18663a9e93043d5e453
Adam S <adam.sarli@gmail.com>
2019-03-15 17:20:49 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
-
+class Book 
+  def initialize(title="And Then There Were None")
+    @title = title 
+    @author = "Agatha Christie"
+    @page_count = 272
+    @genre = "Mystery"
+  end 
+  
+  attr_accessor :title, :author, :page_count, :genre 
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end 
+  
+end 
 
