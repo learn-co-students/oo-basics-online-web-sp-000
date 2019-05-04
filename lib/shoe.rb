4e45bdb51c0a4fbe1276d148c662de59a0711981
Keith Isham <kisham8899@gmail.com>
2019-05-04 15:48:50 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,17 @@
 # Make your shoe class here!
+
+class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition
+# attr_reader :title
+
+      def initialize(brand)
+    @brand = brand
+      end
+
+
+
+  # def turn_page
+  #   puts "Flipping the page...wow, you read fast!"
+  # end
+
+end
