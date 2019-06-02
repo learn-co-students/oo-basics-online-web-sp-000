b725dd1bf70c4edec8ef359bb7ed1fdb2daae13e
Becky Phan <beckyphan@live.com>
2019-06-02 14:24:07 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,14 @@
-
+class Book 
+  
+  def initialize(name)
+    @title = name
+  end
+  
+  attr_accessor :title, :author, :page_count, :genre
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+    
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,13 @@
-# Make your shoe class here!
+class Shoe 
+  def initialize(name)
+    @brand = name
+  end
+  
+  attr_accessor :brand, :color, :size, :material, :condition
+  
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end 
+  
+end
