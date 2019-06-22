903a96c5c60b26d8b1db5ab3599c5df068a86d1c
Connor Cowan <ccowan003@comcast.net>
2019-06-22 00:14:06 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -2,12 +2,12 @@ class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title            #returns title instance variable (property
 
-  def initialize(title)
+  def initialize(title)       #triggers our title argument when we call .new on class
     @title = title
   end
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-  
+
 end
