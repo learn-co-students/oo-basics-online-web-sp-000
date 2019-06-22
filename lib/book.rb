ff1ec2d036f1f389a9d293628980ba53a3104b84
Connor Cowan <ccowan003@comcast.net>
2019-06-22 00:14:43 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -2,7 +2,7 @@ class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title            #returns title instance variable (property
 
-  def initialize(title)       #triggers our title argument when we call .new on class
+  def initialize(title)       #triggers our title argument when we call .new on class upon creation (because every book needs a title)
     @title = title
   end
 
