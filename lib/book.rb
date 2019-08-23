c7b6182f20642b886e862fd232cfe08c93e14ace
Shayan Mohsin <shaymotionfilms@gmail.com>
2019-08-23 20:34:39 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,21 @@
+class Book
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
 
+  GENRES = []
 
+  def initialize(title)
+    @title = title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+    #or GENRES = @genre
+  end
+
+end
