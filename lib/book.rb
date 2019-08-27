a02791b8024fe1e8b7d48529edc4c51b9f6f802e
Michael Kofron <kofronmichael@gmail.com>
2019-08-27 15:45:27 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,42 @@
-
+class Book
+  def initialize(title)
+    @title = title
+  end
+  
+  def title=(book_title)
+    @title = book_title
+  end
+  
+  def title
+    @title
+  end
+  
+  def author=(book_author)
+    @author = book_author
+  end
+  
+  def author
+    @author
+  end
+  
+  def page_count=(book_page_count)
+    @page_count = book_page_count
+  end
+  
+  def page_count 
+    @page_count
+  end
+  
+  def genre=(book_genre)
+    @genre = book_genre
+  end 
+  
+  def genre
+    @genre
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
 
