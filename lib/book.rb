8d52c7c5aa16ab0fa19b8ad5c931013e87d1a33e
Panela Leung <leungpanela@gmail.com>
2019-08-31 15:05:56 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
+class Book
+  #gets me a getter and setter for free
+  attr_accessor :author,
+  :page_count, :genre
+  #gets me a reader and getter for free
+  attr_reader :title
 
+  def initialize(title)
+    @title = title
+  end
 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,54 @@
 # Make your shoe class here!
+class Shoe
+  # attr_reader = :color, :brand, :size,:material, :condition
+  attr_accessor :color, :brand, :size,:material, :condition
+  def initialize (brand)
+    @brand = brand
+  end
+  #
+  # #this is the setter
+  # def color=(color)
+  #   @color = color
+  # end
+  # #this is the getter
+  # def color
+  #   @color
+  # end
+  #
+  # def brand= (brand)
+  # end
+  #
+  # def brand
+  #   @brand
+  # end
+  #
+  # def size= (size)
+  #   @size = size
+  # end
+  #
+  # def size
+  #   @size
+  # end
+  #
+  # def material= (material)
+  #   @material = material
+  # end
+  #
+  # def material
+  #   @material
+  # end
+  #
+  # #this is a setter/writer
+  # def condition= (condition)
+  #   @condition = condition
+  # end
+  # # this is a getter/reader
+  # def condition
+  #   @condition
+  # end
+
+  def cobble
+  puts  "Your shoe is as good as new!"
+  @condition = "new"
+  end
+end
