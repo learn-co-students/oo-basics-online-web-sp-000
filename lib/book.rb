69b369673d8f39d8f96487e954545ff6b6ffe848
wei chen <weichen9083@gmail.com>
2019-09-26 03:30:37 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,34 @@
-
+class Book
+  def initialize(title)
+    @title = title 
+  end 
+  
+  
+  def title
+  @title
+  end 
+  
+  def author=(author_name)
+    @author = author_name
+  end 
+  def author 
+    @author
+  end 
+  
+  def page_count=(num)
+    @page = num
+  end 
+  def page_count
+    @page
+  end 
+  def genre=(genre)
+    @genre = genre
+  end 
+  def genre
+    @genre
+  end 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end 
+end 
 
