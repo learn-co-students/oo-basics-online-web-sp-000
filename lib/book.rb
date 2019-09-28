a8803711cbc56709310256dc8d60c02f4d8f4eae
Blythe Pollard <blytheepollard@gmail.com>
2019-09-28 00:53:29 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,11 +1,12 @@
 class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
   def initialize(title)
     @title = title
   end
-  def title
-    @title
-  end
-  def author=(author)
-    @author = author
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+end
 end
 
