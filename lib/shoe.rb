3d2189aa4df8f082e0734cd0683e91ac77440afc
Bruce Ruffin <bruffin1221@gmail.com>
2019-10-14 19:33:25 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,22 @@
-# Make your shoe class here!
+class Shoe
+
+  def initialize(brand)
+    @brand=brand
+  end
+
+  def brand
+    @brand=brand
+  end
+
+  def brand
+    @brand
+  end
+
+  attr_accessor :brand, :color, :size
+  attr_accessor  :material, :condition
+
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition="new"
+  end
+end
