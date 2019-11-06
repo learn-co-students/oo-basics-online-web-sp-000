31b25419c2b84be82e41335ae6df8e3ac380beaf
Chad Lengefeld <chadlengefeld@gmail.com>
2019-11-06 18:29:59 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,21 @@
+class Book
+    attr_accessor :author, :page_count, :genre
+    attr_reader :title
+    def initialize(title)
+      @title = title
+      #
+    end
+        def title
+          @title
+        end
 
 
+        def turn_page
+          puts "Flipping the page...wow, you read fast!"
+        end
+    end
+
+    book = Book.new("g")
+    book.turn_page
+
+#attr_accessor removes necessary code for getters/setters
