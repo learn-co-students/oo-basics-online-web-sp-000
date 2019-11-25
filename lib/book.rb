4c452c3fb7176e52b7439a45fdcfc32033ae8da6
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-25 19:46:37 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,13 @@
+class Book
+  attr_reader :title
+  attr_accessor :author, :page_count, :genre
 
+  def initialize(title = "And Then There Were None")
+    @title = title
+  end
 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
