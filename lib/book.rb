06f2347ef7d136f1c5ff9bc4f45dc65faab68f52
Stela Capsa <stela.capsa@gmail.com>
2019-12-11 17:48:37 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
-
+class Book 
+  attr_accessor :author, :page_count, :genre
+ def initialize(title = "And Then There Were None")
+   @title = title
+ end
+ def title
+   @title
+ end
+ def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
+ Book.new
+end 
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,19 @@
-# Make your shoe class here!
+class Shoe 
+  attr_accessor :color, :size, :material, :condition, :cobble
+  def initialize (brand = "Adiddas", condition ="new")
+   @brand = brand 
+   @condition = condition
+  end
+  
+  def brand
+    @brand
+  end
+  def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+   end
+   
+Shoe.new
+
+ 
+end 
