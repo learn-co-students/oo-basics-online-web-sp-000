370e2c7df8b3d8c3e54b37f61666c561a2e89da0
Sweet Van Loan <sweet@sweetvanloan.com>
2019-12-14 01:44:48 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,31 @@
 # Make your shoe class here!
+
+class Shoe 
+  
+  attr_accessor :brand, :color, :material, :condition, :shoe 
+  
+  
+  def initialize(shoe)
+    @shoe = shoe 
+  end 
+  
+  def shoe 
+    @shoe 
+  end 
+  
+
+  def size= (num)
+    @size = num 
+  end 
+  
+  def size 
+    @size 
+  end 
+  
+  def cobble(condition)
+    if condition == "old"
+      condition == "new"
+    puts "Your shoe is as good as new!"
+  end 
+  
+end 
