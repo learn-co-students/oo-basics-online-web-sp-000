fa469d220794580287f71044869f197a94b0c26b
Aoune Syed <aoune007@gmail.com>
2020-01-07 06:02:35 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,8 +7,43 @@ class Shoe
   end
 
   def brand
-    @brand 
-
+    @brand
   end
 
+  def color=(color)
+    @color = color
+  end
+
+  def color
+    @color
+  end
+
+  def size=(num)
+      @size = num
+  end
+
+  def size
+    @size
+  end
+
+  def material=(material)
+    @material = material
+  end
+
+  def material
+    @material
+  end
+
+  def condition=(condition)
+    @condition = condition
+  end
+
+  def condition
+    @condition
+  end
+
+def cobble
+  puts "Your shoe is as good as new!"
+  @condition = "new"
+end
 end
