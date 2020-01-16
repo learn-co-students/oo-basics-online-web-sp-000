882fb49094fb92be6ea3964de59d91a9a0453a0b
Luke Sherwood <luke.sherwood55@gmail.com>
2020-01-16 17:57:45 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,14 @@
+class Book
+  
+def initialize(title)
+  @title = title
+  
+end
 
+def turn_page
+  puts "Flipping the page...wow, you read fast!"
+end
 
+attr_accessor :title, :author, :page_count, :genre
+
+end
