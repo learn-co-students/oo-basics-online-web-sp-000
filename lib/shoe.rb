0d4d9927038626b763f9ce7e737e0dcc58a51434
Doak Covington <doakcovington@protonmail.com>
2020-01-24 02:48:47 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
 # Make your shoe class here!
+
+#shoe class
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+end #end of shoe class
