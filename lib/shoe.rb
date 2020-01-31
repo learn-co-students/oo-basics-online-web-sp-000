409c85ea9cee9e47fda1f29c2e1e15aaf14361ec
Hendrick Salomon <hsalomon86@gmail.com>
2020-01-31 04:24:06 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,25 @@
 # Make your shoe class here!
+class Shoe
+  attr_reader :shoe, :brand
+  attr_accessor :color, :size, :material, :condition
+
+  def initialize(shoe, brand="Nike")
+    @shoe = shoe
+    @brand = brand
+  end
+
+  def cobble()
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+
+
+
+  # def brand=(brand="Nike")
+  #   @brand = brand
+  # end
+  #
+  # def brand
+  #   @brand
+  # end
+end
