c7e2bf4a1869548ccec3f26ed0cac093906379fd
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-02-10 17:46:09 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
-# Make your shoe class here!
+class Shoe 
+  def initialize(brand)
+    @brand = brand 
+  end 
+  
+  def brand 
+    @brand 
+  end 
+  
+ attr_accessor :color, :size, :condition, :material  
+  
+  def cobble()
+    self.condition = "new"
+    puts "Your shoe is as good as new!"
+  end 
+end 
