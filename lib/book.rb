6a28c4ec3ca53c3d02b682123efac4889043caa2
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-24 00:36:55 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,42 @@
+class Book
+#title:
+def initialize(title)
+@title = title
+end
 
+def title
+@title
+end
+#author:
+def author=(author)
+  @author = author
+end
 
+def author
+  @author
+end
+#page count:
+def page_count=(num)
+  @page_count = num
+end
+
+def page_count
+  @page_count
+end
+#genre:
+def genre=(genre)
+  @genre = genre
+end
+
+def genre
+  @genre
+end
+#turn page:
+def turn_page
+  puts "Flipping the page...wow, you read fast!"
+end
+ end
+
+Book.new("And Then There Were None")
+book = Book.new("Some Title")
+book.turn_page
