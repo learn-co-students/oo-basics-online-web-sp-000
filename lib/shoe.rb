13845a9816b493b412437b69c870c011cc64015b
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-17 10:14:33 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,19 @@
 # Make your shoe class here!
+class Shoe
+attr_reader :brand
+attr_accessor :color, :material, :size, :condition
+
+def initialize(brand)
+  @brand = brand
+end
+
+def cobble
+  self.condition= "new"
+  #calling the setter method condition=
+  #i can do condition = "new" but i want the truly non-sugar-coated syntax
+  puts "Your shoe is as good as new!"
+end
+
+
+
+end
