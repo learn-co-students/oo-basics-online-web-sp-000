f3a8ae517a287f77463cf5eedb032db37ff59a81
Chris Houghton <chrishoughton2434@gmail.com>
2020-09-21 21:41:25 +0000

    Done.

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 
 
+
 # Object Orientation Basics
 
 ## Description
--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
 
+class Book 
+  
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+  
+  
+  def initialize(title)
+    @title = title
+    @page_count = 0
+  end
+  
+  def turn_page
+    @page_count +=1 
+    puts "Flipping the page...wow, you read fast!"
+  end
 
+end
