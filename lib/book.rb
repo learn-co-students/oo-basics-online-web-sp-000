7af2be1226a4f51e36392e2d28f7a65158957a04
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-09-25 18:00:51 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,36 +1,10 @@
 #Define the book class:
 class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
   #initialize method to allow objects to be created with a title
   def initialize(title)
     @title = title
-  end
-  #allowing books to have a title property
-  def title
-    @title
-  end
-  #author setter
-  def author=(author)
-    @author = author
-  end
-  #author getter
-  def author
-    @author
-  end
-  #page count setter
-  def page_count=(num)
-    @page_count = num
-  end
-  #page count getter
-  def page_count
-    @page_count
-  end
-  #genre setter
-  def genre=(genre)
-    @genre = genre
-  end
-  #genre getter
-  def genre
-    @genre
   end
   #page turner method
   def turn_page
