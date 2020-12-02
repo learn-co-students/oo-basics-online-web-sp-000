753b9f6de06787cc2d67ea2ade0fbba07f179f12
Michael Fuget <michaelfuget@gmail.com>
2020-12-02 07:14:31 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,22 @@
 # Make your shoe class here!
+class Shoe
+  attr_accessor :condition
+  attr_accessor :size
+  attr_accessor :material
+  attr_accessor :color
+  attr_reader :brand
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def title
+    @title 
+  end
+  
+  def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+  end 
+  
+end
