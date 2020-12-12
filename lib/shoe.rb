9c93a58263e113ffb0f3e54f991dc8466fa3171a
Sumer   Gaikwad <sg.comparch@gmail.com>
2020-12-12 21:55:39 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,19 @@
-# Make your shoe class here!
+# Make your shoe class here!class Book
+  
+class Shoe
+  
+  def initialize(brand)
+    
+    @brand = brand
+    
+  end
+  attr_accessor :brand, :color, :size, :material, :condition
+  
+   def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+  
+end
+
+
