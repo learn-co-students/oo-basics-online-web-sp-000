5bbdddc988968e85d0c6d0aad4f2b66ca746d555
Tres Tarwater <trest03@gmail.com>
2020-12-17 00:30:04 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,18 @@
 # Make your shoe class here!
+
+class Shoe
+  
+  attr_accessor :brand, :color, :size, :material, :condition 
+ 
+ 
+  def initialize(brand)
+    @brand = brand
+  end
+  
+  def cobble
+    puts "C"
+    @condition = "new"
+  end
+  
+  
+end
