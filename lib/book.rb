b5011d9477c3afdd74c364155043339a2128929d
HELEN HARMAN <hlharman@bellsouth.net>
2020-12-26 23:37:57 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -2,8 +2,36 @@ class Book
    def initialize (title = "And Then There Were None")
     @title = title 
   end 
+ 
   def title 
     @title 
-    
-end 
+  end 
+
+  def author=(author)
+   @author = author 
+  end 
+  
+  def author
+  @author 
+  end 
+  
+  def page_count=(page_count)
+   @page_count = page_count 
+  end 
+  
+  def page_count
+  @page_count 
+  end 
+  
+  def genre= (genre)
+    @genre = genre 
+    end 
+
+  def genre
+    @genre 
+  end 
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end 
 end 
