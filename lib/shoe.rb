7f5d5cc5f3aa7e782da06bfb4bab113ae56a34fb
Taylor Treece <taylor.treece@protonmail.com>
2021-01-14 11:57:35 -0600

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,15 @@
-# Make your shoe class here!
+class Shoe
+
+    attr_accessor :color, :size, :brand, :material, :condition
+    attr_reader :title
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+
+end
