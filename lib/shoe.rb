122ec332829228a8dbf2802e077ec25c8a300619
Harrison Price <hprice518@gmail.com>
2021-04-14 03:20:32 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -10,10 +10,10 @@ class Shoe
   
   def cobble
     puts "Your shoe is as good as new!"
-  end
   
-  if shoe.condition != "old"
-    puts shoe.cobble
-  end
   
+    if shoe.condition != "old"
+      puts shoe.cobble
+    end
+  end
 end
