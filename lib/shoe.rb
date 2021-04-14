b1213270753ddcdad53c6033da72220f86f04af1
Harrison Price <hprice518@gmail.com>
2021-04-14 02:57:45 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,13 @@
-# Make your shoe class here!
+
+class Shoe
+  
+  attr_accessor :brand, :color, :size, :material, :condition
+  attr_reader 
+  
+
+  
+  def cobble
+    puts "Your shoe is as good as new!"
+  end
+  
+end
