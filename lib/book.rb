3063213a1fcba394b16a67c67e519efeb98c9ad5
Harrison Price <hprice518@gmail.com>
2021-04-14 02:36:17 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -16,12 +16,20 @@ class Book
     @author
   end
   
-  def page_count= (page_count)
-    @page_count = page_count
+  def page_count= (num)
+    @page_count = num
   end
   
   def page_count
     @page_count
+  end
+  
+  def genre= (genre)
+    @genre = genre 
+  end
+  
+  def genre
+    @genre 
   end
   
 end
