97387e15d6e333ea0a0a7a4832f0d2e36d724671
Harrison Price <hprice518@gmail.com>
2021-04-14 03:13:48 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,12 @@
 
 class Shoe
   
-  attr_accessor :brand, :color, :size, :material, :condition
+  attr_accessor 
   attr_reader 
   
-
+  def initialize(brand)
+    @brand = brand
+  end
   
   def cobble
     puts "Your shoe is as good as new!"
