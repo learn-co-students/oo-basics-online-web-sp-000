e0917b801a283c2b69e39fbcf4ba0d92995a9d5c
pekellogg <pekellogg@hotmail.com>
2021-04-27 23:21:55 -0700

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,25 @@
-# Make your shoe class here!
+class Shoe
+    attr_reader :brand
+    attr_accessor :color, :size, :material, :condition
+
+    def initialize(brand)
+        @brand = brand
+        @color = color
+        @size = size
+        @material = material
+        @condition = condition
+    end
+
+    def cobble
+        self.condition = "new"
+        puts "Your shoe is as good as new!"
+    end
+end
+
+puts louboutins = Shoe.new("Christian Louboutin")
+puts louboutins.brand
+puts louboutins.color = "black"
+puts louboutins.size = 11
+puts louboutins.material = "vegan_leather"
+puts louboutins.condition = "new"
+puts louboutins.condition = "tattered"
