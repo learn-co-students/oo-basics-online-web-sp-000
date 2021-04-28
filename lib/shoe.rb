8f7fe93e90576d658978795c972f2d3d54f1b1e7
pekellogg <pekellogg@hotmail.com>
2021-04-27 23:32:24 -0700

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,10 +4,6 @@ class Shoe
 
     def initialize(brand)
         @brand = brand
-        @color = color
-        @size = size
-        @material = material
-        @condition = condition
     end
 
     def cobble
