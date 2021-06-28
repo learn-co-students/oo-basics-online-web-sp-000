6c7e7109db32663626e078a2c532bdbcc32236c4
Lj <tnedutsl457892@gmail.com>
2021-06-28 01:37:51 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
+  BRANDS = []
+  def initialize(brand)
+    @brand = brand
+    BRANDS << @brand
+    BRANDS.uniq!
+  end
+
+  def cobble
+    self.condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+end
